4bc4d299327dd3d8b94cd1e0abec51fb5e9e0412
paul_hazy <pavelpv8@gmail.com>
2021-03-03 00:40:35 +0400

    update css

--- app/app.py
+++ app/app.py
@@ -1,6 +1,2 @@
-from flask import Flask
-from config import Config
-
-app = Flask(__name__)
-#get configuration from config.py class Config
-app.config.from_object(Config)
+class Config(object):
+    DEBUG = True
--- app/main.py
+++ app/main.py
@@ -1,21 +1,29 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, send_from_directory
 from flask import render_template, request
-from app import app
+from app import Config
+
+app = Flask(__name__)
 
 @app.route("/")
-def main():
+def main_index():
     return render_template("index.html")
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 #заюзать позже
 #@app.route("/<name>")
 #def user(name):
 #    return f"Hello {name}"
 
+
+
 if __name__ == '__main__':
+    app.config.from_object(Config)
     app.run()
-
-
